40fb873dc1a5fb281e0a699602c1084e21b15b31
MiguelM Duarte <miguelm.duarte@blip.pt>
2020-06-27 19:11:22 +0100

    more updates

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@ docker build --tag jenkins-with-node:1.0 .
 
 # Run container
 docker run -p 8080:8080 -p 50000:50000 -v /Users/duartem2/Documents/TECH/repos/github/miguelmatosduarte/jenkins-CI-CD-meetmehalfway/jenkins_home:/var/jenkins_home -v /Users/duartem2/Documents/TECH/repos/github/miguelmatosduarte/jenkins-CI-CD-meetmehalfway/.ssh:/root/.ssh jenkins-with-node:1.0
+
